be51e48923a7088be377b375d684f2efb5983bf9
sanghunBaek <bsh56592@gmail.com>
2022-11-14 22:42:13 +0900

    requirement one

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +13,13 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @DisplayName("split test case")
+    @Test
+    void split() {
+        List<String> actual = Arrays.asList("1,2".split(","));
+        assertThat(actual).contains("1","2");
+        assertThat(actual).containsExactly("1","2");
     }
 }
